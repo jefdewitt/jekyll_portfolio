c66b448ec779064e879b1952c43b1e469362bf39
Jef DeWitt <jefdewitt@gmail.com>
2016-11-25 19:02:40 -0500

    adds angular ctrl and new typewrite image

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
-angular.module('angularApp', []);
+angular.module('angularApp', [])
+
+.controller('firstCtrl', function($scope) {
+    $scope.helloWorld = function() {
+        document.write('You just pressed the button.');
+    };
+});
