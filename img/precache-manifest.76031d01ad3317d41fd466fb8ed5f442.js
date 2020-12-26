a3378604772d99f059a0599996b9c4c62ea763fe
Jef Dewitt <jef.dewitt@twinspires.com>
2020-12-26 07:59:16 -0500

    Adds react app.

---
+++ img/precache-manifest.76031d01ad3317d41fd466fb8ed5f442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba9e16e2bd641860af7fe751c124959",
+    "url": "/img/index.html"
+  },
+  {
+    "revision": "ed04ddfe28aef4fafe45",
+    "url": "/img/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "0b0a6b492b88233a4cb2",
+    "url": "/img/static/css/main.220913f7.chunk.css"
+  },
+  {
+    "revision": "ed04ddfe28aef4fafe45",
+    "url": "/img/static/js/2.ee4e6497.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/img/static/js/2.ee4e6497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0a6b492b88233a4cb2",
+    "url": "/img/static/js/main.bc624288.chunk.js"
+  },
+  {
+    "revision": "a1533c9faf987446d184",
+    "url": "/img/static/js/runtime-main.5e18f28c.js"
+  }
+]);
