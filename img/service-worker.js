406622b691816cf591291737714a18b1f048e6e0
Jef Dewitt <jef.dewitt@twinspires.com>
2021-04-19 06:10:29 -0400

    New img app build.

--- img/service-worker.js
+++ img/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/img/precache-manifest.66cee5f45b2b8e99b00dec50043de5d2.js"
+  "/img/precache-manifest.67630860a7d501f3a7c148e5fa9d9376.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/img/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
